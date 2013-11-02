17d9d1c5204e817960ac64b7b751f87379ea82c0
Willian van der Velde <willian@pythonheads.nl>
2013-11-02 18:17:44 +0100

    Create an admin user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 if Rails.env.development?
-  user = User.create!(email: 'example@example.com', password: 'password123')
-  puts "User 'example@example.com' with password 'password123' created"
+  user = User.create!(email: 'example@example.com', password: 'password123', admin: true)
+  puts "Admin user 'example@example.com' with password 'password123' created"
 end
